170986f67388ac85c2c0e085859c2a8ff1971dda
Explorer <vitaljan@gmail.com>
2018-10-09 16:13:13 +0300

    2018-10-09

--- test.js
+++ test.js
@@ -1,43 +1,7 @@
 
-function rnd(min, max) { // возвращает случайное целое число от min до max
-    return Math.round(Math.random()*(max-min))+min;
+function play(){
+    var hihi = new Audio ('http://explorer.org.ua/game/up1.wav');
+    hihi.src='up1.wav';
+    hihi.play();
+    alert('play');
 }
-
-var spriteCount=5;
-var spriteView = 3;
-
-var sprite={
-    exist:false,
-    init:function() {
-        this.exist = false;
-    }
-};
-var sprites = [];
-for (n=0; n<spriteCount; n++) {
-    sprites.push(new (sprite.init));
-}
-for (var n = 1; n <= spriteView; n++) {
-    var j = rnd(0,spriteCount-1);
-    j
-
-          while (sprites[j].exist) {
-            j++;
-            if (j == spriteCount) j = 0;
-         }
-
-          sprites[j].exist = true;
-
-        }
- console.log(sprites);
-
- var j = rnd(0,spriteCount-1);
- j
- 
- while (sprites[j].exist) {
-   j++;
-   if (j == spriteCount) j = 0;
- }
-j
-
-
-
